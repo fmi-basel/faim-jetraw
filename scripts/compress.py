2a7c7d943238507110e68af3377b98e16f7e5f5e
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2022-07-04 16:09:42 +0200

    Add boiler plate.

--- scripts/compress.py
+++ scripts/compress.py
@@ -1,2 +1,30 @@
+import argparse
+import configparser
+
+from prefect import Flow
+
+
+def config2kwargs(config):
+    return {"num_workers": config["DEFAULT"]["num_workers"]}
+
+
+with Flow("Compress") as flow:
+    pass
+
+from prefect.executors import LocalDaskExecutor
+
 if __name__ == "__main__":
-    raise NotImplementedError
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config")
+    args = parser.parse_args()
+
+    config = configparser.ConfigParser()
+    config.read(args.config)
+
+    kwargs = config2kwargs(config)
+
+    flow.executor = LocalDaskExecutor(num_workers=kwargs["num_workers"])
+
+    kwargs.pop("num_workers")
+
+    flow.run(parameters=kwargs)
